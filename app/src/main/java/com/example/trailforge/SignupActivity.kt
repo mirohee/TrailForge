f93c86cfc204ec8dfd906c655a8e1a02d07dbe4f
veikkope <veikko.pelkonen@metropolia.fi>
2024-11-07 17:32:10 +0200

    Simple Layout

---
+++ app/src/main/java/com/example/trailforge/SignupActivity.kt
@@ -0,0 +1,14 @@
+package com.example.trailforge
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        // You can add logic here for handling signup
+    }
+}
